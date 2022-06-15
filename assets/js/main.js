e0ef9586179abfdef3af8581097ad28b22dbf1ff
kimjuno97 <jh241093@gmail.com>
2022-06-15 13:12:28 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,3 +206,10 @@
 					});
 
 })(jQuery);
+
+const audio = document.getElementById('player');
+        const audioVolume = document.getElementById('volume-control');
+
+        audioVolume.addEventListener("change", function(e) {
+        audio.volume = this.value/10;
+        })
