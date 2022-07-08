02baad7fb8cec89ce4ccc05e15dd7b0c7c78d220
kimjuno97 <jh241093@gmail.com>
2022-07-08 15:21:36 +0900

    myimg.js부분 수정
    전역변수로 선언했던 부분을 수정하였다

--- assets/js/myimg.js
+++ assets/js/myimg.js
@@ -1,15 +1,23 @@
 const myImg = ["my1.jpg","my2.jpg","my3.jpg","my4.jpg","my5.jpg","my6.jpg","my7.jpg","my8.jpg","my9.jpg","my10.jpg"];
 const imgPlace = document.querySelector(".my-img")
-let currentIndex = 0;
 
-function imgPick(){
-    if(currentIndex<myImg.length-1){
-    imgPlace.src = `images/my/${myImg[currentIndex]}`;
-    currentIndex++;
-    }else{
-        imgPlace.src = `images/my/${myImg[currentIndex]}`;
-        currentIndex = 0;   
-    }
+//클로저
+function makeCounter(){
+    let currentIndex = 0;
+
+    return function () {
+        if(currentIndex == myImg.length-1){
+           return currentIndex = 0;
+       }
+        return ++currentIndex;
+       
+    };
+    
+}
+let counterIndex = makeCounter();
+
+function imgPick(){    
+    imgPlace.src = `images/my/${myImg[counterIndex()]}`;
 }
 imgPick();
 setInterval(imgPick,3500);
